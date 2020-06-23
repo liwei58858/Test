bd0bb9c60bec1dec7dacf0ded0d0e801b4ed340b
李威 <liwei58858@163.com>
2020-06-23 10:35:55 +0800

    测试中。。提交主分支

--- Test/Test/XiaoXiao.h
+++ Test/Test/XiaoXiao.h
@@ -1,16 +1,16 @@
 //
-//  YZBaseTextField.h
+//  XiaoXiao.h
 //  Test
 //
 //  Created by li.w on 2020/6/23.
 //  Copyright © 2020 云政网络. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface YZBaseTextField : UITextField
+@interface XiaoXiao : NSObject
 
 @end
 
---
+++ Test/Test/XiaoXiao.m
@@ -0,0 +1,13 @@
+//
+//  XiaoXiao.m
+//  Test
+//
+//  Created by li.w on 2020/6/23.
+//  Copyright © 2020 云政网络. All rights reserved.
+//
+
+#import "XiaoXiao.h"
+
+@implementation XiaoXiao
+
+@end
